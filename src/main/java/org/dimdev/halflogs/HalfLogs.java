bd2b50e9ddb3d5475d5040c8384ed6ea99368401
Robijnvogel <robijn.bird@gmail.com>
2018-08-30 03:41:24 +0200

    Updated to Rift 1.0.4-65
    -Updated to Rift 1.0.4-65
    -Updated mappings to snapshot_20180826
    -Resolved mappings conflicts
    -Fixed a HalfLogs model mistake from my previous commit

--- src/main/java/org/dimdev/halflogs/HalfLogs.java
+++ src/main/java/org/dimdev/halflogs/HalfLogs.java
@@ -26,18 +26,18 @@ public class HalfLogs implements BlockAdder, ItemAdder {
     private static Supplier<BlockStairs> LOG_STAIRS = () -> {
         Block block = Blocks.OAK_LOG;
 
-        return new BlockStairs(block.getDefaultState(), Block.Builder.create(Material.WOOD, MapColor.WOOD).hardnessAndResistance(2.0F, 3.0F).soundType(SoundType.WOOD)) {
+        return new BlockStairs(block.getDefaultState(), Block.Builder.create(Material.WOOD, MapColor.WOOD).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)) {
             @Override
-            protected void addPropertiesToBuilder(StateContainer.Builder<Block,IBlockState> p_Builder) {
-                super.addPropertiesToBuilder(p_Builder);
-                p_Builder.addProperties(AXIS);
+            protected void fillStateContainer(StateContainer.Builder<Block,IBlockState> p_Builder) {
+                super.fillStateContainer(p_Builder);
+                p_Builder.add(AXIS);
             }
 
             @Override
-            public IBlockState getBlockToPlaceOnUse(BlockItemUseContext p_getBlockToPlaceOnUse_1_)
+            public IBlockState getStateForPlacement(BlockItemUseContext p_getBlockToPlaceOnUse_1_)
             {
-                IBlockState iblockstate = super.getBlockToPlaceOnUse(p_getBlockToPlaceOnUse_1_);
-                iblockstate = iblockstate.withProperty(AXIS, p_getBlockToPlaceOnUse_1_.func_196000_l().getAxis());
+                IBlockState iblockstate = super.getStateForPlacement(p_getBlockToPlaceOnUse_1_);
+                iblockstate = iblockstate.withProperty(AXIS, p_getBlockToPlaceOnUse_1_.func_196010_d().getAxis());
                 return iblockstate;
             }
         };
@@ -45,18 +45,18 @@ public class HalfLogs implements BlockAdder, ItemAdder {
 
     private static Supplier<BlockSlab> LOG_SLABS = () -> {
 
-        return new BlockSlab(Block.Builder.create(Material.WOOD, MapColor.WOOD).hardnessAndResistance(2.0F, 3.0F).soundType(SoundType.WOOD)) {
+        return new BlockSlab(Block.Builder.create(Material.WOOD, MapColor.WOOD).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)) {
             @Override
-            protected void addPropertiesToBuilder(StateContainer.Builder<Block,IBlockState> p_Builder) {
-                super.addPropertiesToBuilder(p_Builder);
-                p_Builder.addProperties(AXIS);
+            protected void fillStateContainer(StateContainer.Builder<Block,IBlockState> p_Builder) {
+                super.fillStateContainer(p_Builder);
+                p_Builder.add(AXIS);
             }
 
             @Override
-            public IBlockState getBlockToPlaceOnUse(BlockItemUseContext p_getBlockToPlaceOnUse_1_)
+            public IBlockState getStateForPlacement(BlockItemUseContext p_getBlockToPlaceOnUse_1_)
             {
-                IBlockState iblockstate = super.getBlockToPlaceOnUse(p_getBlockToPlaceOnUse_1_);
-                iblockstate = iblockstate.withProperty(AXIS, p_getBlockToPlaceOnUse_1_.func_196000_l().getAxis());
+                IBlockState iblockstate = super.getStateForPlacement(p_getBlockToPlaceOnUse_1_);
+                iblockstate = iblockstate.withProperty(AXIS, p_getBlockToPlaceOnUse_1_.func_196010_d().getAxis());
                 return iblockstate;
             }
         };
@@ -95,66 +95,66 @@ public class HalfLogs implements BlockAdder, ItemAdder {
     @Override
     public void registerBlocks() {
         //Slabs
-        Block.registerBlock(new ResourceLocation("halflogs:oak_log_slab"), OAK_LOG_SLAB);
-        Block.registerBlock(new ResourceLocation("halflogs:spruce_log_slab"), SPRUCE_LOG_SLAB);
-        Block.registerBlock(new ResourceLocation("halflogs:birch_log_slab"), BIRCH_LOG_SLAB);
-        Block.registerBlock(new ResourceLocation("halflogs:jungle_log_slab"), JUNGLE_LOG_SLAB);
-        Block.registerBlock(new ResourceLocation("halflogs:acacia_log_slab"), ACACIA_LOG_SLAB);
-        Block.registerBlock(new ResourceLocation("halflogs:dark_oak_log_slab"), DARK_OAK_LOG_SLAB);
+        Block.register(new ResourceLocation("halflogs:oak_log_slab"), OAK_LOG_SLAB);
+        Block.register(new ResourceLocation("halflogs:spruce_log_slab"), SPRUCE_LOG_SLAB);
+        Block.register(new ResourceLocation("halflogs:birch_log_slab"), BIRCH_LOG_SLAB);
+        Block.register(new ResourceLocation("halflogs:jungle_log_slab"), JUNGLE_LOG_SLAB);
+        Block.register(new ResourceLocation("halflogs:acacia_log_slab"), ACACIA_LOG_SLAB);
+        Block.register(new ResourceLocation("halflogs:dark_oak_log_slab"), DARK_OAK_LOG_SLAB);
         //Stripped
-        Block.registerBlock(new ResourceLocation("halflogs:stripped_oak_log_slab"), STRIPPED_OAK_LOG_SLAB);
-        Block.registerBlock(new ResourceLocation("halflogs:stripped_spruce_log_slab"), STRIPPED_SPRUCE_LOG_SLAB);
-        Block.registerBlock(new ResourceLocation("halflogs:stripped_birch_log_slab"), STRIPPED_BIRCH_LOG_SLAB);
-        Block.registerBlock(new ResourceLocation("halflogs:stripped_jungle_log_slab"), STRIPPED_JUNGLE_LOG_SLAB);
-        Block.registerBlock(new ResourceLocation("halflogs:stripped_acacia_log_slab"), STRIPPED_ACACIA_LOG_SLAB);
-        Block.registerBlock(new ResourceLocation("halflogs:stripped_dark_oak_log_slab"), STRIPPED_DARK_OAK_LOG_SLAB);
+        Block.register(new ResourceLocation("halflogs:stripped_oak_log_slab"), STRIPPED_OAK_LOG_SLAB);
+        Block.register(new ResourceLocation("halflogs:stripped_spruce_log_slab"), STRIPPED_SPRUCE_LOG_SLAB);
+        Block.register(new ResourceLocation("halflogs:stripped_birch_log_slab"), STRIPPED_BIRCH_LOG_SLAB);
+        Block.register(new ResourceLocation("halflogs:stripped_jungle_log_slab"), STRIPPED_JUNGLE_LOG_SLAB);
+        Block.register(new ResourceLocation("halflogs:stripped_acacia_log_slab"), STRIPPED_ACACIA_LOG_SLAB);
+        Block.register(new ResourceLocation("halflogs:stripped_dark_oak_log_slab"), STRIPPED_DARK_OAK_LOG_SLAB);
 
         //Stairs
-        Block.registerBlock(new ResourceLocation("halflogs:oak_log_stairs"), OAK_LOG_STAIRS);
-        Block.registerBlock(new ResourceLocation("halflogs:spruce_log_stairs"), SPRUCE_LOG_STAIRS);
-        Block.registerBlock(new ResourceLocation("halflogs:birch_log_stairs"), BIRCH_LOG_STAIRS);
-        Block.registerBlock(new ResourceLocation("halflogs:jungle_log_stairs"), JUNGLE_LOG_STAIRS);
-        Block.registerBlock(new ResourceLocation("halflogs:acacia_log_stairs"), ACACIA_LOG_STAIRS);
-        Block.registerBlock(new ResourceLocation("halflogs:dark_oak_log_stairs"), DARK_OAK_LOG_STAIRS);
+        Block.register(new ResourceLocation("halflogs:oak_log_stairs"), OAK_LOG_STAIRS);
+        Block.register(new ResourceLocation("halflogs:spruce_log_stairs"), SPRUCE_LOG_STAIRS);
+        Block.register(new ResourceLocation("halflogs:birch_log_stairs"), BIRCH_LOG_STAIRS);
+        Block.register(new ResourceLocation("halflogs:jungle_log_stairs"), JUNGLE_LOG_STAIRS);
+        Block.register(new ResourceLocation("halflogs:acacia_log_stairs"), ACACIA_LOG_STAIRS);
+        Block.register(new ResourceLocation("halflogs:dark_oak_log_stairs"), DARK_OAK_LOG_STAIRS);
         //Stripped
-        Block.registerBlock(new ResourceLocation("halflogs:stripped_oak_log_stairs"), STRIPPED_OAK_LOG_STAIRS);
-        Block.registerBlock(new ResourceLocation("halflogs:stripped_spruce_log_stairs"), STRIPPED_SPRUCE_LOG_STAIRS);
-        Block.registerBlock(new ResourceLocation("halflogs:stripped_birch_log_stairs"), STRIPPED_BIRCH_LOG_STAIRS);
-        Block.registerBlock(new ResourceLocation("halflogs:stripped_jungle_log_stairs"), STRIPPED_JUNGLE_LOG_STAIRS);
-        Block.registerBlock(new ResourceLocation("halflogs:stripped_acacia_log_stairs"), STRIPPED_ACACIA_LOG_STAIRS);
-        Block.registerBlock(new ResourceLocation("halflogs:stripped_dark_oak_log_stairs"), STRIPPED_DARK_OAK_LOG_STAIRS);
+        Block.register(new ResourceLocation("halflogs:stripped_oak_log_stairs"), STRIPPED_OAK_LOG_STAIRS);
+        Block.register(new ResourceLocation("halflogs:stripped_spruce_log_stairs"), STRIPPED_SPRUCE_LOG_STAIRS);
+        Block.register(new ResourceLocation("halflogs:stripped_birch_log_stairs"), STRIPPED_BIRCH_LOG_STAIRS);
+        Block.register(new ResourceLocation("halflogs:stripped_jungle_log_stairs"), STRIPPED_JUNGLE_LOG_STAIRS);
+        Block.register(new ResourceLocation("halflogs:stripped_acacia_log_stairs"), STRIPPED_ACACIA_LOG_STAIRS);
+        Block.register(new ResourceLocation("halflogs:stripped_dark_oak_log_stairs"), STRIPPED_DARK_OAK_LOG_STAIRS);
     }
 
     @Override
     public void registerItems() {
         //Slabs
-        Item.registerItemBlock(OAK_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(SPRUCE_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(BIRCH_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(JUNGLE_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(ACACIA_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(DARK_OAK_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
+        Item.register(OAK_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
+        Item.register(SPRUCE_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
+        Item.register(BIRCH_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
+        Item.register(JUNGLE_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
+        Item.register(ACACIA_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
+        Item.register(DARK_OAK_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
         //Stripped
-        Item.registerItemBlock(STRIPPED_OAK_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(STRIPPED_SPRUCE_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(STRIPPED_BIRCH_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(STRIPPED_JUNGLE_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(STRIPPED_ACACIA_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(STRIPPED_DARK_OAK_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
+        Item.register(STRIPPED_OAK_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
+        Item.register(STRIPPED_SPRUCE_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
+        Item.register(STRIPPED_BIRCH_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
+        Item.register(STRIPPED_JUNGLE_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
+        Item.register(STRIPPED_ACACIA_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
+        Item.register(STRIPPED_DARK_OAK_LOG_SLAB, ItemGroup.BUILDING_BLOCKS);
 
         //Stairs
-        Item.registerItemBlock(OAK_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(SPRUCE_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(BIRCH_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(JUNGLE_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(ACACIA_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(DARK_OAK_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
+        Item.register(OAK_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
+        Item.register(SPRUCE_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
+        Item.register(BIRCH_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
+        Item.register(JUNGLE_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
+        Item.register(ACACIA_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
+        Item.register(DARK_OAK_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
         //Stripped
-        Item.registerItemBlock(STRIPPED_OAK_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(STRIPPED_SPRUCE_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(STRIPPED_BIRCH_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(STRIPPED_JUNGLE_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(STRIPPED_ACACIA_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
-        Item.registerItemBlock(STRIPPED_DARK_OAK_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
+        Item.register(STRIPPED_OAK_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
+        Item.register(STRIPPED_SPRUCE_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
+        Item.register(STRIPPED_BIRCH_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
+        Item.register(STRIPPED_JUNGLE_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
+        Item.register(STRIPPED_ACACIA_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
+        Item.register(STRIPPED_DARK_OAK_LOG_STAIRS, ItemGroup.BUILDING_BLOCKS);
     }
 }
